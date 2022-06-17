ba81924e6e02a167fd11cd61ce65bd62e089e01a
Manuel Raygoza <Manny.raygoza@gmail.com>
2022-06-17 16:22:58 -0700

    WIP: making changes to index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,23 @@ pizza.price = [22, 18, 14];
 
 let myNewPizza = new Pizza(pizza.size, pizza.toppings, pizza.price);
 
-Pizza.prototype.chooseTopping = function () {
-    this.toppings = chooseTopping(pizza.toppings);
+Pizza.prototype.chooseToppings = function () {
+    this.toppings = chooseToppings(pizza.toppings);
 }
 
 Pizza.prototype.chooseSize = function () {
   this.size = chooseSize(pizza.size);
+}
+
+Pizza.prototype.completePizza = function (chooseToppings, chooseSize) {
+  if (pizza.chooseToppings >= 3) {
+    return "Toppings Complete!";
+  } else if 
+    return "Just Cheese?"; { 
+  } else if  (pizza.chooseSize === size) { 
+    return (size);
+  }
+  
 }
 
 //User Interface (Ui)Logic:
