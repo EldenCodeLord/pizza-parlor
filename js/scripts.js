d193a92a259a693d19dd463fbcfd39bc64acfdfc
Manuel Raygoza <Manny.raygoza@gmail.com>
2022-06-17 13:17:05 -0700

    WIP: Added Pizza.prototype.() to business logic. Test: passed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
 //Business Logic:
-
-let pizza = {};
-pizza.size = ["Large", "Medium", "Small"];
-pizza.toppings = ["Pineapple", "Pepperoni", "Peperoccini", "Anchovies", "Black Olives"];
-pizza.price = [22, 18, 14]; 
-
 function Pizza (size, toppings, price) {
   this.size = size;
   this.toppings = toppings;
   this.price= price;
 }
+let pizza = {};
+pizza.size = ["Large", "Medium", "Small"];
+pizza.toppings = ["Pineapple", "Pepperoni", "Peperoccini", "Anchovies", "Black Olives"];
+pizza.price = [22, 18, 14]; 
 
 let myNewPizza = new Pizza(pizza.size, pizza.toppings, pizza.price);
-myNewPizza = 
 
-//User Interface Logic:
+Pizza.prototype.chooseTopping = function () {
+    this.toppings = chooseTopping(pizza.toppings);
+}
+
+Pizza.prototype.chooseSize = function () {
+  this.size = chooseSize(pizza.size);
+}
+
+//User Interface (Ui)Logic:
 
 $()
